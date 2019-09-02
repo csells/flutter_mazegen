d982f5e233503c7efd2fc7e814d41010bfbf7190
Chris Sells <csells@sellsbrothers.com>
2019-09-01 19:43:26 -0700

    updated windows, macos and ios sub-dirs

--- macos/MainFlutterWindow.swift
+++ macos/MainFlutterWindow.swift
@@ -15,9 +15,9 @@
 import Cocoa
 import FlutterMacOS
 
-class ExampleWindow: NSWindow {
+class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
-    let flutterViewController = FLEViewController.init()
+    let flutterViewController = FlutterViewController.init()
     let windowFrame = self.frame
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
